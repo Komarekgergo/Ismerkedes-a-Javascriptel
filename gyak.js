d8e06069b737e17db3312a26484473ae27197372
komarekgergo <komarekgergo@freemail.hu>
2020-10-16 14:46:40 +0200

    elso commit

---
+++ gyak.js
@@ -0,0 +1,85 @@
+'use strict';
+
+/*
+let szam=125;
+//Hibakereső felület.
+console.log(szam);
+//DOM (Document Object Model) segítsével.
+document.getElementById('pelda').inmerText=szam;
+//BOM (Browser Object model) segítségével.
+//Modális ablakban (modal window) jelenik meg.
+window.alert(szam);
+*/
+
+/*window.confirm modális függgvény, boolean értékkel tér vissza */
+/*
+let beenged= window.confirm('Elmúltál már 18 éve?');
+
+if(beenged < 18){
+    document.getElementById('pelda').innerText= 'Nem mehetsz be, mert kicsi vagy';
+}
+
+else{
+    document.getElementById('pelda').innerText='Elég nagy vagy, bemehetsz';
+}*/
+
+/*window.prompt modális függgvény,  szövegértékkel (string) tér vissza */
+
+//let beenged = window.prompt('Hány éves vagy?',18);
+
+/* Típuskonverzió */
+// let kor = Number(beenged)
+//let kor= parseInt(beenged)
+
+/*let kor = +beenged;
+
+if (kor<18){
+
+document.getElementById('pelda').innerText= 'Nem mehetsz be, mert kicsi vagy';
+
+}
+
+else{
+     document.getElementById('pelda').innerText='Elég nagy vagy, bemehetsz';
+
+} */
+
+/*let maiNap = window.prompt('A hét hanyadik napja van ?'; 1);
+
+let nap = +maiNap;
+
+if (nap == 1){
+
+    document.getElementById('pelda').innerText= 'A mai nap hétfő';
+}
+
+else if (nap == 2){
+
+    document.getElementById('pelda').innerText= 'A mai nap kedd';
+}
+ else if (nap == 3){
+
+    document.getElementById('pelda').innerText= 'A mai nap szerda';
+}
+ 
+else if (nap == 4){
+
+    document.getElementById('pelda').innerText= 'A mai nap csütörtök';
+}
+
+else if (nap == 5){
+
+    document.getElementById('pelda').innerText= 'A mai nap péntek';
+}
+else if (nap == 6){
+
+    document.getElementById('pelda').innerText= 'A mai nap szombat';
+}
+else if (nap == 7){
+
+    document.getElementById('pelda').innerText= 'A mai nap vasárnap';
+}*/
+
+
+
+
